82913a8f01517cc8e875363a750b39a59d251fbc
yasemin <minied@gmx.net>
2014-07-05 15:34:12 +0200

    bla

--- src/testpackage/Testclass.java
+++ src/testpackage/Testclass.java
@@ -3,7 +3,7 @@ package testpackage;
 public class Testclass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world2014");
+		System.out.println("Hello world2015");
 
 	}
 
