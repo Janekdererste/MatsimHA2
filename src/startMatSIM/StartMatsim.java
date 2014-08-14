5fad76c6b4500edb8aedf39c6067d6f0a58aef60
Janek <janekl@web.de>
2014-08-14 11:00:27 +0200

    Vorbereitung für zweiten Durchlauf

--- src/startMatSIM/StartMatsim.java
+++ src/startMatSIM/StartMatsim.java
@@ -6,8 +6,22 @@ import org.matsim.core.config.Config;
 
 public class StartMatsim {
 
+	private static final String DEFAULT_CONFIG = "input/config.xml";
+	
 	public static void main(String[] args) {
-		Config config = ConfigUtils.loadConfig("input/config.xml");
+		
+		String path;
+		if(args.length == 2)
+		{
+			if(args[0].equalsIgnoreCase("-config"))
+				path = args[1];
+			else
+				path = DEFAULT_CONFIG;				
+		}
+		else
+			path = DEFAULT_CONFIG;		
+		
+		Config config = ConfigUtils.loadConfig(path);
 		Controler controler = new Controler(config);
 		controler.setOverwriteFiles(true);
 		controler.run();
